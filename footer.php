e9403071070a7a9d1691d631f8ed33da8d2f17a7
Andressa de Faria Souza <afariasouza@outlook.com>
2023-11-09 10:54:43 -0300

    sections e quebra html para php

---
+++ footer.php
@@ -0,0 +1,24 @@
+</main>
+
+<footer class="" id="bottom">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-10 my-5">
+        <p class="fs-6 text-center">
+          2023 <span class="mx-2">&mdash;</span>
+          <a href="https://portal.campinas.sp.gov.br/secretaria/administracao" target="_blank" rel="noopener noreferrer"
+            >Prefeitura de Campinas - SMA</a
+          >
+          &copy; Todos os direitos reservados.
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- JAVASCRIPT -->
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/tooltip.js"></script>
+<script src="js/back-to-top.js"></script>
+</body>
+</html>
