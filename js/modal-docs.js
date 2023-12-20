73b81cc1e9628765d322181030028df00b3b2ac9
Andressa de Faria Souza <afariasouza@outlook.com>
2023-12-20 16:31:40 -0300

    Conserta o Título do Modal

--- js/modal-docs.js
+++ js/modal-docs.js
@@ -2,11 +2,12 @@ const modalDetalhes = document.getElementById('modalDetalhes');
 if (modalDetalhes) {
   modalDetalhes.addEventListener('show.bs.modal', (event) => {
     // Extrai o Número do Empenho
-    const modalNumber = document.querySelector('.numero-empenho');
+    const modalLink = event.relatedTarget;
+    const modalNumber = modalLink.getAttribute('data-bs-content');
 
     // Muda o título do Modal para o Número do Empenho
     const modalTitle = modalDetalhes.querySelector('.modal-title');
 
-    modalTitle.textContent = modalNumber.textContent;
+    modalTitle.textContent = modalNumber;
   });
 }
