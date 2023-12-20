7bb43833098304890e92da3ebe5e439e281d3d68
Andressa de Faria Souza <afariasouza@outlook.com>
2023-12-20 16:33:59 -0300

    Alterações no Link do Modal

--- js/modal-docs.js
+++ js/modal-docs.js
@@ -3,11 +3,10 @@ if (modalDetalhes) {
   modalDetalhes.addEventListener('show.bs.modal', (event) => {
     // Extrai o Número do Empenho
     const modalLink = event.relatedTarget;
-    const modalNumber = modalLink.getAttribute('data-bs-content');
 
     // Muda o título do Modal para o Número do Empenho
     const modalTitle = modalDetalhes.querySelector('.modal-title');
 
-    modalTitle.textContent = modalNumber;
+    modalTitle.textContent = modalLink.textContent;
   });
 }
