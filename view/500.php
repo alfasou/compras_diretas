0c0ec025937f3c894ad61532d8f0bc3726f69d2d
Andressa de Faria Souza <afariasouza@outlook.com>
2023-12-21 11:10:55 -0300

    Cria página de Erro 500

---
+++ view/500.php
@@ -0,0 +1,10 @@
+<section id="error500">
+  <div class="container">
+    <div class="row justify-content-center align-items-center">
+    <div class="col-10 text-center">
+      <h1>500</h1>
+      <h2>Erro Interno do Servidor</h2>
+    </div>
+    </div>
+  </div>
+</section>
