1da9c956bc95c2ce307ead37654ea2a189c42a90
Andressa de Faria Souza <afariasouza@outlook.com>
2024-04-12 11:56:50 -0300

    Adequação do Layout

--- js/modal.js
+++
@@ -1,6 +0,0 @@
-const myModal = document.getElementById('modalEmpenho');
-const myInput = document.getElementById('myInput');
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus();
-});
